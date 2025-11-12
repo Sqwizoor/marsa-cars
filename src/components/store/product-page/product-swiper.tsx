4cad0a9b73863ed51a0579db5f746f24b7103b5f
alex banele <alexsouthflow@gmail.com>
2025-11-12 06:24:31 +0200

    uutttttttt

--- src/components/store/product-page/product-swiper.tsx
+++ src/components/store/product-page/product-swiper.tsx
@@ -1,6 +1,6 @@
 "use client";
 // React, Next.js
-import { Dispatch, SetStateAction, useState } from "react";
+import { Dispatch, SetStateAction } from "react";
 import Image from "next/image";
 
 // Image zoom
@@ -21,15 +21,8 @@ export default function ProductSwiper({
   activeImage: ProductVariantImage | null;
   setActiveImage: Dispatch<SetStateAction<ProductVariantImage | null>>;
 }) {
-  const [isLoading, setIsLoading] = useState(false);
-
   // If no images are provided, exit early and don't render anything
   if (!images) return;
-
-  const handleImageChange = (img: ProductVariantImage) => {
-    setIsLoading(true);
-    setActiveImage(img);
-  };
 
   return (
     <div className="relative">
@@ -47,7 +40,7 @@ export default function ProductSwiper({
                     : false,
                 }
               )}
-              onMouseEnter={() => handleImageChange(img)}
+              onMouseEnter={() => setActiveImage(img)}
             >
               <Image
                 src={img.url}
@@ -59,26 +52,12 @@ export default function ProductSwiper({
             </div>
           ))}
         </div>
-        {/* Image view with fixed dimensions */}
-        <div className="relative rounded-lg overflow-hidden w-full h-[400px] md:h-[500px] xl:h-[550px] 2xl:h-[600px] 2xl:w-[600px] bg-gray-50 flex-shrink-0">
-          {isLoading && (
-            <div className="absolute inset-0 bg-gray-50 animate-pulse z-10" />
-          )}
-          {/* Hidden image to track loading */}
-          {activeImage && (
-            <Image
-              src={activeImage.url}
-              alt=""
-              width={1}
-              height={1}
-              className="hidden"
-              onLoad={() => setIsLoading(false)}
-            />
-          )}
+        {/* Image view */}
+        <div className="relative rounded-lg overflow-hidden">
           <ImageZoom
             src={activeImage ? activeImage.url : ""}
             zoom={200}
-            className="!w-full !h-full rounded-lg"
+            className="!w-full rounded-lg"
           />
         </div>
       </div>
