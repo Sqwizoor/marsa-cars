0e25f4fd4eac7f30a56e69724da3bdb190c600ea
alex banele <alexsouthflow@gmail.com>
2026-01-06 14:10:10 +0200

    we almost therre

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -49,7 +49,7 @@ export default async function HomePage() {
                 className="cursor-pointer hidden min-[1465px]:block bg-cover bg-center bg-no-repeat rounded-md h-[400px] sm:h-[500px] min-[1465px]:h-auto w-full"
                 style={{
                   backgroundImage:
-                    "url(/assets/images/ads/new-side.png)",
+                    "url(/assets/images/sideline/new-side.png)",
                 }}
               />
               {/* Middle */}
